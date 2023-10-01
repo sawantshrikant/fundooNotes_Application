e83fe55e9a3d7897e1b43cb95dd219dc7b442979
Shrikant <sawantshrikant07@gmail.com>
2023-10-01 12:51:41 +0530

    [Shrikant] Add: in user.services use destructure to get output on postman only email and firstname for userRegistration

--- fundooNotes/fundooNotes_Application/src/services/user.service.js
+++ fundooNotes/fundooNotes_Application/src/services/user.service.js
@@ -17,25 +17,42 @@ export const userRegistration = async(body) => {
 
     data = await User.create(body);
 
-    data = {
-      firstName: data.firstName,
-      email: data.email
-    }
+   //const {firstName , email} = data;
+   const {email , firstName} = data;
+
+
+   data = {email , firstName}
+
+    // data = {
+    //   firstName: data.firstName,
+    //   email: data.email
+    // }
    }
 
    return data;
 }
 
 export const userLogin = async(body) => {
-  const checkForUserCredentials = await User.findOne({email: body.email,password: body.password});
+  const checkForUserCredentials = await User.findOne({email: body.email});
+  
   var data;
   if(checkForUserCredentials == null){
     throw new Error("User not registered")
   }else{
-    data = {
-      firstName: checkForUserCredentials.firstName,
-      email: checkForUserCredentials.email,
+    const match = await bcrypt.compare(body.password, checkForUserCredentials.password);
+
+    if(match) {
+
+      const {email,firstName} = checkForUserCredentials;
+
+      data = {email,firstName};
+                                                                           
+      }
+     else {
+      throw new Error("Wrong Password")
     }
+
+    
   }
   return data;
 }
