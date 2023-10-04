2c33b87c56fe2755e6aca19a2fb9116b47dd0a24
Shrikant <sawantshrikant07@gmail.com>
2023-10-04 17:49:17 +0530

    [Shrikant] Add: refactor code using Second Secret_Key for forgot

--- fundooNotes/fundooNotes_Application/src/middlewares/auth.middleware.js
+++ fundooNotes/fundooNotes_Application/src/middlewares/auth.middleware.js
@@ -30,4 +30,25 @@ export const userAuth = async (req, res, next) => {
   }
 };
 
+export const ForgotAuth = async (req, res, next) => {
+  try {
+    let bearerToken = req.header('Authorization');
+
+    if (!bearerToken)
+      throw {
+        code: HttpStatus.BAD_REQUEST,
+        message: 'Authorization token is required',
+      };
+    bearerToken = bearerToken.split(' ');
+
+    var user = await jwt.verify(bearerToken[1], process.env.Secret_KeyForForgot);
+
+    req.body.email = user.email;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
 
